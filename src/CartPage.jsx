2d518d31bbead5c981b125746a57b293052dd0b1
Qrchy <130994567+Qrchy@users.noreply.github.com>
2024-03-13 21:44:10 +0100

    non functional home page, store and cart

---
+++ src/CartPage.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+
+function CartPage() {
+  return (
+    <div className="StorePage">
+      <Navbar />
+    </div>
+  );
+}
+
+export default CartPage;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StorePage from './StorePage';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import CartPage from './CartPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/store',
+    element: <StorePage />
+  },
+  {
+    path: '/cart',
+    element: <CartPage />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
